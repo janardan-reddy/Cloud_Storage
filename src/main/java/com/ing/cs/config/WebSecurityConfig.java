56aeb4de73c52155b4b31283d18e274998854a3c
janardan-reddy <118843802+janardan-reddy@users.noreply.github.com>
2023-07-25 11:45:00 +0530

    Disabling CSRF for testing of functionality

--- src/main/java/com/ing/cs/config/WebSecurityConfig.java
+++ src/main/java/com/ing/cs/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -22,7 +23,8 @@ public class WebSecurityConfig {
         http
                 .authorizeHttpRequests((authorize) -> {
                     authorize.anyRequest().authenticated();
-                }).httpBasic(Customizer.withDefaults());
+                }).httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable);
         return http.build();
     }
 
